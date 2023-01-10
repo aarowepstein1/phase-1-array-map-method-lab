130e1520915ef10c1f892da4448a98c183b82b7f
Aarow Epstein <aarowepstein1@gmail.com>
2023-01-09 20:49:31 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map( (letter) => {
+    const arr = letter.split(' ');
+
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+
+    const str2 = arr.join(' ');
+
+    return str2
+  })
+   
 }
